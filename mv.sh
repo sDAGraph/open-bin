d03a39b2ed6f7a833d8344a8e2d49301ec5a7565
Tien <skyholy6@yahoo.com.tw>
2019-04-12 17:53:51 +0800

    renew bin file

--- mv.sh
+++ mv.sh
@@ -2,7 +2,7 @@ rm -rf core
 go build ~/go/src/lurcury/core/
 mv ~/go/src/lurcury/core/core ~/open-bin/
 rm -rf client
-go build ~/go/src/lurcury/core/
+go build ~/go/src/lurcury/client/
 mv ~/go/src/lurcury/client/client ~/open-bin/
 cd ~/open-bin/
 git add .
