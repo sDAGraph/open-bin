eea4291a04440b2989214f69296e0e38615186a7
TXien <skyholy6@yahoo.com.tw>
2019-04-12 17:51:40 +0800

    Update mv.sh

--- mv.sh
+++ mv.sh
@@ -1,2 +1,10 @@
+rm -rf core
+go build ./go/src/lurcury/core/
 mv ~/go/src/lurcury/core/core ~/open-bin/
+rm -rf client
+go build ./go/src/lurcury/core/
 mv ~/go/src/lurcury/client/client ~/open-bin/
+cd ~/open-bin/
+git add .
+git commit -m "renew bin file"
+git push
