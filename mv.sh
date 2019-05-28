2d7b7f8a65e2787158631baa5a14bbf4c23e24a6
Tien <skyholy6@yahoo.com.tw>
2019-05-28 19:11:10 +0800

    renew bin file

--- mv.sh
+++ mv.sh
@@ -1,11 +1,11 @@
 #rm -rf core
 go build ~/go/src/lurcury/core/
-#mv ~/go/src/lurcury/core/core ~/open-bin/
+mv ~/go/src/lurcury/core/core ~/open-bin/
 #rm -rf client
 go build ~/go/src/lurcury/client/
-#mv ~/go/src/lurcury/client/client ~/open-bin/
+mv ~/go/src/lurcury/client/client ~/open-bin/
 #cd ~/open-bin/
-go build ~/go/src/vvm/
+go build ~/go/src/lurcury/vvm/
 git add .
 git commit -m "renew bin file"
 git push
